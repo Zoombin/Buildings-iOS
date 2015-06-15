27cb4911e9fc04027c6efeb97100ecac4a758f41
Bin Zhang <30135878@qq.com>
2015-06-15 12:29:16 +0800

    Change local host.

--- Buildings/Buildings/Header.h
+++ Buildings/Buildings/Header.h
@@ -11,7 +11,7 @@
 
 #define API_HOST @"http://218.4.117.11:8093/"
 #define WEB_HOST @"http://112.124.98.9/buildings-web/html/"
-//#define WEB_HOST @"http://127.0.0.1/html/"
+//#define WEB_HOST @"http://127.0.0.1/Buildings-Web/html/"
 #define APP_SCHEME @"XXBuildings"
 #define OPEN_CONTACTS @"openContacts"
 #define PARAMETERS @"?id=9d79a67e96d643789612d267495a7ab8&login=1&type=1&dtype&ios"
